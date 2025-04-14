31dbeddd493e899de82f9d8cba9c285b9602dc7b
mzml <hi@mzml.io>
2025-04-14 14:34:38 -0600

    first commit

---
+++ specs/search_accessibility.spec
@@ -0,0 +1,15 @@
+# Search accessibility
+Tags: search,
+
+The student can use the Know Your Program and Search By Keyword to find information.
+
+* User goes to the homepage
+* Run accessibility audit
+
+## Find Your Program Search
+
+The student can search program using keywords
+
+
+* User searches for "laser"
+* User sees "1" program found
---
+++ tests/search_accessibility_implementation.js
@@ -0,0 +1,69 @@
+/* globals gauge*/
+"use strict";
+const path = require('path');
+const {
+    openBrowser,
+    setViewPort,
+    accessibility,
+    write,
+    closeBrowser,
+    goto,
+    press,
+    screenshot,
+    above,
+    click,
+    checkBox,
+    listItem,
+    toLeftOf,
+    link,
+    text,
+    into,
+    textBox,
+    evaluate,
+    emulateDevice,
+    scrollDown,
+    waitFor
+} = require('taiko');
+const assert = require("assert");
+const headless = process.env.headless_chrome.toLowerCase() === 'true';
+
+beforeSuite(async () => {
+    await openBrowser({ headless: headless });
+    // await setViewPort({width:312, height:810});
+    await emulateDevice("iPhone X");
+});
+
+afterSuite(async () => {
+    await closeBrowser();
+});
+
+// Return a screenshot file name
+gauge.customScreenshotWriter = async function () {
+    const screenshotFilePath = path.join(process.env['gauge_screenshots_dir'],
+        `screenshot-${process.hrtime.bigint()}.png`);
+
+    await screenshot({
+        path: screenshotFilePath
+    });
+    return path.basename(screenshotFilePath);
+};
+
+step("User goes to the homepage", async function () {
+    await goto("www.frontrange.edu/");
+});
+
+step("Run accessibility audit", async function () {
+    const audit = await accessibility.runAudit();
+    await console.log(audit.violations);
+});
+
+step("User searches for <program_name>", async function(program_name) {
+    // await scrollDown("Browse All Programs");
+	await write(program_name, into(textBox({name: "search"})));
+    await press('Enter');
+    await waitFor(5000);
+});
+
+step("User sees <results> program found", async function(results) {
+	throw 'Unimplemented Step';
+});
